b06def072cf3cdb0468e9f1493fde1acb70d39dc
Pedro <goncalves.miguel@aluno.ifsp.edu.br>
2025-03-18 16:29:38 -0300

    opções

---
+++ lista.py
@@ -0,0 +1,14 @@
+agenda = []
+
+def exibir_menu():
+    print("AGENDA DE CONTATOS")
+    print("1. Adicionar contato")
+    print("2. Listar contatos")
+    print("3. Buscar contato")
+    print("4. Atualizar contato")
+    print("5. Remover contato")
+    print("6. Favoritar/Desfavoritar contato")
+    print("7. Listar contatos favoritos")
+    print("8. Sair")
+    return input("Escolha uma opção: ")
+
